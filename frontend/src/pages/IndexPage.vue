319510e00929961256a239a23217da0c11e0ae58
juzeon <812312770@qq.com>
2023-10-20 21:17:23 +0800

    add more logic to chat page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import {onMounted, ref} from "vue"
 
+let modeList = ['Creative', 'Balanced', 'Precise']
+let mode = ref('Creative')
+let backendList = ref([])
+let backend = ref('')
+let localeList = ['zh-CN', 'en-US', 'en-IE', 'en-GB']
+let locale = ref('zh-CN')
+let presetList = ref([])
+let preset = ref('')
+onMounted(() => {
+
+})
 </script>
 
 <template>
@@ -8,19 +20,39 @@
       <p class="mb-5">Chat Context:</p>
       <v-spacer></v-spacer>
       <div class="d-flex">
-        <v-select color="primary" label="Backend" density="compact" class="mx-2"></v-select>
-        <v-select color="primary" label="Mode" density="compact" class="mx-2"></v-select>
-        <v-select color="primary" label="Locale" density="compact" class="mx-2"></v-select>
-        <v-select color="primary" label="Preset" density="compact" class="mx-2"></v-select>
+        <v-select v-model="backend" :items="backendList" color="primary" label="Backend" density="compact"
+                  class="mx-2"></v-select>
+        <v-select v-model="mode" :items="modeList" color="primary" label="Mode" density="compact"
+                  class="mx-2"></v-select>
+        <v-select v-model="locale" :items="localeList" color="primary" label="Locale" density="compact"
+                  class="mx-2"></v-select>
+        <v-select v-model="preset" :items="presetList" color="primary" label="Preset" density="compact"
+                  class="mx-2"></v-select>
       </div>
       <v-btn color="primary" class="mb-5 ml-2">Reset</v-btn>
     </div>
     <div class="flex-grow-1" style="height: 50vh;overflow: auto">
-      <v-textarea no-resize auto-grow :rows="20"></v-textarea>
+      <v-textarea class="ma-0" no-resize auto-grow :rows="20"></v-textarea>
     </div>
-    <div class="mb-3">Tool bar</div>
+    <div class="my-2 d-flex">
+      <p>Follow-up User Input:</p>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" density="compact" class="mx-1">Image</v-btn>
+      <v-btn color="primary" density="compact" class="mx-1">Document</v-btn>
+      <v-btn color="primary" density="compact" class="mx-1">Browse</v-btn>
+      <v-btn color="primary" density="compact" class="mx-1">Revoke</v-btn>
+      <v-menu>
+        <template #activator="{props}">
+          <v-btn color="primary" density="compact" append-icon="mdi-menu-down" v-bind="props" class="mx-1">Quick</v-btn>
+        </template>
+        <v-list density="compact">
+          <v-list-item>test</v-list-item>
+        </v-list>
+      </v-menu>
+      <v-btn color="primary" density="compact" class="mx-1">Send</v-btn>
+    </div>
     <div style="overflow: auto">
-      <v-textarea :rows="5" no-resize></v-textarea>
+      <v-textarea class="ma-0" :rows="5" no-resize></v-textarea>
     </div>
   </div>
 </template>
