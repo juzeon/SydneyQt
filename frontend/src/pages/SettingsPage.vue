8f794aeb4910eb6b2fddf9b53277d4c6d5a8c22d
juzeon <812312770@qq.com>
2023-11-25 16:06:33 +0800

    fix components color

--- frontend/src/pages/SettingsPage.vue
+++ frontend/src/pages/SettingsPage.vue
@@ -48,35 +48,37 @@ function onDarkModeSwitch() {
               <v-tooltip text="Enter a HTTP proxy (e.g. http://127.0.0.1:7890). Leave blank to disable proxy."
                          location="bottom">
                 <template #activator="{props}">
-                  <v-text-field label="Proxy" v-model="config.proxy" v-bind="props"></v-text-field>
+                  <v-text-field color="primary" label="Proxy" v-model="config.proxy" v-bind="props"></v-text-field>
                 </template>
               </v-tooltip>
               <v-tooltip
                   text="FQDN for the websocket endpoint of Sydney, without any prefixes (protocol names) or suffixes (slashes)."
                   location="bottom">
                 <template #activator="{props}">
-                  <v-text-field label="Wss Domain" v-model="config.wss_domain" v-bind="props"></v-text-field>
+                  <v-text-field color="primary" label="Wss Domain" v-model="config.wss_domain"
+                                v-bind="props"></v-text-field>
                 </template>
               </v-tooltip>
             </v-card-text>
           </v-card>
           <v-card title="Display" class="my-3">
             <v-card-text>
-              <v-switch label="Dark Mode" :model-value="config.dark_mode"
+              <v-switch color="primary" label="Dark Mode" :model-value="config.dark_mode"
                         @update:model-value="onDarkModeSwitch"></v-switch>
-              <v-switch label="No Suggestion" v-model="config.no_suggestion"></v-switch>
+              <v-switch color="primary" label="No Suggestion" v-model="config.no_suggestion"></v-switch>
               <v-tooltip text="Default: SF" location="bottom">
                 <template #activator="{props}">
-                  <v-text-field v-bind="props" label="Font Family" v-model="config.font_family"></v-text-field>
+                  <v-text-field color="primary" v-bind="props" label="Font Family"
+                                v-model="config.font_family"></v-text-field>
                 </template>
               </v-tooltip>
-              <v-slider min="10" max="30" step="1" thumb-label="always" v-model="config.font_size"
+              <v-slider color="primary" min="10" max="30" step="1" thumb-label="always" v-model="config.font_size"
                         label="Font Size"></v-slider>
               <div class="text-center mb-3" :style="fontStyle">Font Example</div>
               <!-- TODO v-slider tooltip -->
               <v-tooltip text="Height of the textarea of user input, in vh." location="bottom">
                 <template #activator="{props}">
-                  <v-slider v-bind="props" step="1" min="10" max="60" label="User Input Textarea Height"
+                  <v-slider color="primary" v-bind="props" step="1" min="10" max="60" label="User Input Textarea Height"
                             v-model="config.stretch_factor"
                             thumb-label="always"></v-slider>
                 </template>
@@ -85,7 +87,7 @@ function onDarkModeSwitch() {
           </v-card>
           <v-card title="Accessibility" class="my-3">
             <v-card-text>
-              <v-select label="Enter Mode" v-model="config.enter_mode"
+              <v-select color="primary" label="Enter Mode" v-model="config.enter_mode"
                         :items="['Enter','Ctrl+Enter']"></v-select>
             </v-card-text>
           </v-card>
