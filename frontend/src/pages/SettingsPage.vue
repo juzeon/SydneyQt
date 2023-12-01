21931f289845bacf372e3fe7ce56bcb4f46fc49d
juzeon <812312770@qq.com>
2023-12-01 17:20:56 +0800

    add backend settings

--- frontend/src/pages/SettingsPage.vue
+++ frontend/src/pages/SettingsPage.vue
@@ -8,6 +8,7 @@ import {computed, onMounted, ref} from "vue"
 import {useTheme} from "vuetify"
 import {main} from "../../wailsjs/go/models"
 import Preset = main.Preset
+import OpenAIBackend = main.OpenAIBackend
 
 let theme = useTheme()
 let router = useRouter()
@@ -18,6 +19,7 @@ onMounted(() => {
   fetchingSettings().then(() => {
     loading.value = false
     activePreset.value = config.value.presets[0]
+    activeOpenaiBackendName.value = config.value.open_ai_backends[0].name
   })
 })
 let fontStyle = computed(() => {
@@ -139,6 +141,56 @@ function confirmRenamePreset() {
   }
   renamePresetInstance.value!.name = renamePresetName.value
   renamePresetDialog.value = false
+}
+
+let activeOpenaiBackendName = ref('')
+let renameBackendName = ref('')
+let isRenamingBackend = ref(false)
+let renameBackendError = ref('')
+
+function onRenameBackend() {
+  renameBackendError.value = ''
+  renameBackendName.value = activeOpenaiBackendName.value
+  isRenamingBackend.value = true
+}
+
+function confirmRenameBackend() {
+  if (renameBackendName.value === '') {
+    return
+  }
+  if (config.value.open_ai_backends.find(v => v.name === renameBackendName.value) || renameBackendName.value === 'Sydney') {
+    renameBackendError.value = 'Backend name already exists.'
+    return
+  }
+  config.value.open_ai_backends.find(v => v.name === activeOpenaiBackendName.value)!.name = renameBackendName.value
+  activeOpenaiBackendName.value = renameBackendName.value
+  isRenamingBackend.value = false
+}
+
+function addOpenaiBackend() {
+  let backend = <OpenAIBackend>Object.assign({},
+      config.value.open_ai_backends.find(v => v.name === activeOpenaiBackendName.value)!)
+  backend.name = 'OpenAI ' + uuidV4().split('-')[0]
+  config.value.open_ai_backends.push(backend)
+  activeOpenaiBackendName.value = backend.name
+}
+
+function deleteOpenaiBackend(backend: OpenAIBackend) {
+  if (config.value.open_ai_backends.length <= 1) {
+    return
+  }
+  activeOpenaiBackendName.value = config.value.open_ai_backends[0].name
+  config.value.open_ai_backends = config.value.open_ai_backends.filter(v => v !== backend)
+}
+
+function checkOpenaiEndpoint(val: string) {
+  if (!val.endsWith('/v1')) {
+    return 'The endpoint is expected to end with /v1'
+  }
+  if (!val.startsWith('http')) {
+    return 'The endpoint is expected to start with http'
+  }
+  return true
 }
 </script>
 
@@ -338,6 +390,59 @@ function confirmRenamePreset() {
                     </v-card-actions>
                   </v-card>
                 </v-dialog>
+              </v-card>
+              <v-card class="my-3">
+                <v-card-title>OpenAI Backends</v-card-title>
+                <v-card-subtitle>Allow you to invoke OpenAI ChatGPT API.
+                  You can set multiple configurations and choose a backend from the chat page.
+                </v-card-subtitle>
+                <v-card-text>
+                  <div class="d-flex">
+                    <v-tabs class="flex-grow-1" show-arrows v-model="activeOpenaiBackendName" color="primary"
+                            :disabled="isRenamingBackend">
+                      <v-tab v-for="backend in config.open_ai_backends" :value="backend.name">{{ backend.name }}</v-tab>
+                    </v-tabs>
+                    <v-btn variant="text" icon color="primary" @click="addOpenaiBackend" :disabled="isRenamingBackend">
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                  </div>
+                  <v-window v-model="activeOpenaiBackendName">
+                    <v-window-item v-for="backend in config.open_ai_backends" :value="backend.name">
+                      <div class="mx-3 my-3">
+                        <div class="d-flex align-center">
+                          <v-text-field v-if="isRenamingBackend" label="Name" v-model="renameBackendName"
+                                        color="primary" :error-messages="renameBackendError"></v-text-field>
+                          <v-text-field v-else label="Name" :model-value="backend.name" color="primary"
+                                        :disabled="true"></v-text-field>
+                          <div v-if="isRenamingBackend" class="d-flex mx-3 mb-3">
+                            <v-btn icon color="primary" variant="text" class="mx-1" @click="confirmRenameBackend">
+                              <v-icon>mdi-check</v-icon>
+                            </v-btn>
+                            <v-btn icon color="primary" variant="text" class="mx-1" @click="isRenamingBackend=false">
+                              <v-icon>mdi-close</v-icon>
+                            </v-btn>
+                          </div>
+                          <v-btn v-else icon color="primary" variant="text" class="mx-3 mb-3" @click="onRenameBackend">
+                            <v-icon>mdi-pen</v-icon>
+                          </v-btn>
+                        </div>
+                        <v-text-field label="Endpoint" :rules="[checkOpenaiEndpoint]" v-model="backend.openai_endpoint"
+                                      color="primary"></v-text-field>
+                        <v-text-field label="Key" v-model="backend.openai_key" color="primary"></v-text-field>
+                        <v-text-field label="Model" v-model="backend.openai_short_model" color="primary"></v-text-field>
+                        <v-slider label="Temperature" v-model="backend.openai_temperature" min="0" max="2"
+                                  step="0.1" color="primary" thumb-label="always"></v-slider>
+                        <div class="d-flex my-3">
+                          <v-spacer></v-spacer>
+                          <v-btn icon color="red" variant="text" :disabled="isRenamingBackend"
+                                 @click="deleteOpenaiBackend(backend)">
+                            <v-icon>mdi-delete</v-icon>
+                          </v-btn>
+                        </div>
+                      </div>
+                    </v-window-item>
+                  </v-window>
+                </v-card-text>
               </v-card>
             </v-card-text>
           </v-card>
