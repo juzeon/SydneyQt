506b40122eee7d43975f0167c18314a484af8ac2
juzeon <812312770@qq.com>
2023-12-06 20:36:41 +0800

    impl sydney image creation

---
+++ sydney/image.go
@@ -0,0 +1,57 @@
+package sydney
+
+import (
+	"errors"
+	"github.com/go-resty/resty/v2"
+	"log/slog"
+	"net/url"
+	"regexp"
+	"sydneyqt/util"
+	"time"
+)
+
+func (o *Sydney) GenerateImage(generativeImage GenerativeImage) (GenerateImageResult, error) {
+	start := time.Now()
+	var empty GenerateImageResult
+	hClient, err := util.MakeHTTPClient(o.proxy, 0)
+	if err != nil {
+		return empty, err
+	}
+	client := resty.New().SetTransport(hClient.Transport).
+		SetHeaders(o.headersCreateImage).
+		SetTimeout(15 * time.Second)
+	resp, err := client.R().Get(generativeImage.URL)
+	if err != nil && !errors.Is(err, resty.ErrAutoRedirectDisabled) {
+		return empty, err
+	}
+	arr := regexp.MustCompile("/images/create/async/results/(.*?)\\?").FindStringSubmatch(string(resp.Body()))
+	if len(arr) < 2 {
+		return empty, errors.New("cannot find image creation result")
+	}
+	resultID := arr[1]
+	re := regexp.MustCompile(`<img class="mimg".*?src="(.*?)"`)
+	u := "https://www.bing.com/images/create/async/results/" + resultID +
+		"?q=" + url.QueryEscape(generativeImage.Text) + "&partner=sydney&showselective=1&IID=images.as"
+	slog.Info("Result URL", "v", u)
+	for i := 0; i < 15; i++ {
+		time.Sleep(3 * time.Second)
+		resp, err := client.R().Get(u)
+		if err != nil {
+			return empty, err
+		}
+		var imageURLs []string
+		arr := re.FindAllStringSubmatch(string(resp.Body()), -1)
+		if len(arr) == 0 {
+			slog.Info("No matched images currently", "body", string(resp.Body()))
+			continue
+		}
+		for _, match := range arr {
+			imageURLs = append(imageURLs, match[1])
+		}
+		return GenerateImageResult{
+			ImageURLs: imageURLs,
+			Duration:  time.Now().Sub(start),
+		}, nil
+	}
+	return empty, errors.New("image creation timeout")
+}
--- sydney/sydney.go
+++ sydney/sydney.go
@@ -24,6 +24,7 @@ type Sydney struct {
 	allowedMessageTypes       []string
 	headers                   map[string]string
 	headersCreateConversation map[string]string
+	headersCreateImage        map[string]string
 }
 
 func NewSydney(debug bool, cookies map[string]string, proxy string,
@@ -176,6 +177,18 @@ func NewSydney(debug bool, cookies map[string]string, proxy string,
 			"user-agent":                  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46",
 			"x-edge-shopping-flag":        "1",
 			"x-forwarded-for":             forwardedIP,
+		},
+		headersCreateImage: map[string]string{
+			"authority":                 "www.bing.com",
+			"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+			"accept-language":           "en-US,en;q=0.9",
+			"cache-control":             "no-cache",
+			"referer":                   "https://www.bing.com/search?q=Bing+AI&showconv=1",
+			"upgrade-insecure-requests": "1",
+			"user-agent":                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46",
+			"x-forwarded-for":           forwardedIP,
+			"Sec-Fetch-Dest":            "iframe",
+			"Cookie":                    util.FormatCookieString(cookies),
 		},
 	}
 }
