3414bd79b5745a09e2500e41342cba5f421c9107
juzeon <812312770@qq.com>
2023-12-06 17:45:51 +0800

    open all links externally

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,21 @@
 <script lang="ts" setup>
+import {BrowserOpenURL} from "../wailsjs/runtime"
+
+document.body.addEventListener('click', function (e) {
+  // @ts-ignore
+  if (e.target && e.target.nodeName == 'A' && e.target.href) {
+    // @ts-ignore
+    const url = e.target.href
+    if (
+        !url.startsWith('http://#') &&
+        !url.startsWith('file://') &&
+        !url.startsWith('http://wails.localhost:')
+    ) {
+      e.preventDefault()
+      BrowserOpenURL(url)
+    }
+  }
+})
 </script>
 
 <template>
