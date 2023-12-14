2c3c82a599cdabbaa4e0098e0e799563ed4a7525
juzeon <812312770@qq.com>
2023-12-14 16:57:21 +0800

    add theme color picker

--- frontend/src/components/settings/ThemeTextField.vue
+++ frontend/src/components/settings/ThemeTextField.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {shadeColor} from "../../helper"
 import {useTheme} from "vuetify"
+import {ref} from "vue"
 
 let props = defineProps<{
   themeColor: string,
@@ -22,12 +23,32 @@ function onChangeThemeColor(val: string) {
 function checkThemeColor(val: string): boolean {
   return /^#[0-9A-Fa-f]{6}$/i.test(val)
 }
+
+let colorDialog = ref(false)
+
 </script>
 
 <template>
-  <v-text-field label="Theme Color" color="primary" :model-value="themeColor"
-                @update:model-value="onChangeThemeColor"
-                hint="Must be a valid hex color; default value: #FF9800"></v-text-field>
+  <div class="d-flex align-center">
+    <v-text-field label="Theme Color" color="primary" :model-value="themeColor"
+                  @update:model-value="onChangeThemeColor"
+                  hint="Must be a valid hex color; default value: #FF9800"></v-text-field>
+    <v-btn icon variant="tonal" color="primary" class="mb-5 ml-3" @click="colorDialog=true">
+      <v-icon>mdi-select-color</v-icon>
+    </v-btn>
+    <v-dialog v-model="colorDialog" max-width="350">
+      <v-card title="Pick a Theme Color">
+        <v-card-text>
+          <v-color-picker :hide-inputs="true" :model-value="themeColor" @update:model-value="onChangeThemeColor"
+                          :show-swatches="true"></v-color-picker>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" variant="text" @click="colorDialog=false">Done</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <style scoped>
