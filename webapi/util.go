a33aed77d47667304295f1609d4ff36ad3fa915c
Randall Fitzgerald <randall@dasaku.net>
2023-12-30 09:42:58 -0800

    Make sure the file exists AND ends in .json, otherwise, Header Cookie String

--- webapi/util.go
+++ webapi/util.go
@@ -5,12 +5,15 @@ import (
 	"io/ioutil"
 	"fmt"
 	"encoding/json"
+	"os"
 )
 
 func ParseCookies(cookiesStr string) map[string]string {
 	cookies := map[string]string{}
 	
-	if strings.HasSuffix(cookiesStr, ".json") {
+	// use the os.Stat function to make sure the file exists and make sure it points to a .json file
+	// else load it is a cookieString from a header
+	if _, err := os.Stat(cookiesStr); err == nil && strings.HasSuffix(cookiesStr, ".json") {
 		data, err := ioutil.ReadFile(cookiesStr)
 		if err != nil {
 			fmt.Println("error:", err)
@@ -28,17 +31,17 @@ func ParseCookies(cookiesStr string) map[string]string {
 		}
 		
 		for _, item := range items {
-        	cookies[item.Name] = item.Value
-    	}
-	} else {
-		
-	for _, cookie := range strings.Split(cookiesStr, ";") {
-		parts := strings.Split(cookie, "=")
-		if len(parts) == 2 {
-			cookies[parts[0]] = parts[1]
+			cookies[item.Name] = item.Value
 		}
-	}	
+	} else {
+		for _, cookie := range strings.Split(cookiesStr, ";") {
+			parts := strings.Split(cookie, "=")
+			if len(parts) == 2 {
+			cookies[parts[0]] = parts[1]
+			}
+		}
 	}
+
 	
 	return cookies
 }
