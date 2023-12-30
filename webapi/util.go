0e61eae908c66a4a9406208146b2ccfb03238d04
Randall Fitzgerald <randall@dasaku.net>
2023-12-30 10:07:19 -0800

    Reverting util.go changes

--- webapi/util.go
+++ webapi/util.go
@@ -2,41 +2,7 @@ package main
 
 import (
 	"strings"
-	"encoding/json"
-	"os"
-	"errors"
 )
-
-type FileCookie struct {
-	Domain         string      `json:"domain"`
-	ExpirationDate float64     `json:"expirationDate"`
-	HostOnly       bool        `json:"hostOnly"`
-	HttpOnly       bool        `json:"httpOnly"`
-	Name           string      `json:"name"`
-	Path           string      `json:"path"`
-	SameSite       string      `json:"sameSite"`
-	Secure         bool        `json:"secure"`
-	Session        bool        `json:"session"`
-	StoreId        interface{} `json:"storeId"`
-	Value          string      `json:"value"`
-}
-
-func ReadCookiesFile() (map[string]string, error) {
-	res := map[string]string{}
-	v, err := os.ReadFile("cookies.json")
-	if err != nil {
-		return res, err
-	}
-	var cookies []FileCookie
-	err = json.Unmarshal(v, &cookies)
-	if err != nil {
-		return res, errors.New("failed to json.Unmarshal content of cookie file")
-	}
-	for _, cookie := range cookies {
-		res[cookie.Name] = cookie.Value
-	}
-	return res, nil
-}
 
 func ParseCookies(cookiesStr string) map[string]string {
 	cookies := map[string]string{}
@@ -46,6 +12,5 @@ func ParseCookies(cookiesStr string) map[string]string {
 			cookies[parts[0]] = parts[1]
 		}
 	}
-	
 	return cookies
 }
