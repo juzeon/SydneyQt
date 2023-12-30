56b4080b3de7f2f149ac32c1f09134b5c25dcfbc
Randall Fitzgerald <randall@dasaku.net>
2023-12-30 09:56:29 -0800

    Update util.go to add ReadCookiesFile()

--- webapi/util.go
+++ webapi/util.go
@@ -2,46 +2,50 @@ package main
 
 import (
 	"strings"
-	"io/ioutil"
-	"fmt"
 	"encoding/json"
 	"os"
+	"errors"
 )
+
+type FileCookie struct {
+	Domain         string      `json:"domain"`
+	ExpirationDate float64     `json:"expirationDate"`
+	HostOnly       bool        `json:"hostOnly"`
+	HttpOnly       bool        `json:"httpOnly"`
+	Name           string      `json:"name"`
+	Path           string      `json:"path"`
+	SameSite       string      `json:"sameSite"`
+	Secure         bool        `json:"secure"`
+	Session        bool        `json:"session"`
+	StoreId        interface{} `json:"storeId"`
+	Value          string      `json:"value"`
+}
+
+func ReadCookiesFile() (map[string]string, error) {
+	res := map[string]string{}
+	v, err := os.ReadFile("cookies.json")
+	if err != nil {
+		return res, err
+	}
+	var cookies []FileCookie
+	err = json.Unmarshal(v, &cookies)
+	if err != nil {
+		return res, errors.New("failed to json.Unmarshal content of cookie file")
+	}
+	for _, cookie := range cookies {
+		res[cookie.Name] = cookie.Value
+	}
+	return res, nil
+}
 
 func ParseCookies(cookiesStr string) map[string]string {
 	cookies := map[string]string{}
-	
-	// use the os.Stat function to make sure the file exists and make sure it points to a .json file
-	// else load it is a cookieString from a header
-	if _, err := os.Stat(cookiesStr); err == nil && strings.HasSuffix(cookiesStr, ".json") {
-		data, err := ioutil.ReadFile(cookiesStr)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-
-		type Item struct {
-			Name  string `json:"name"`
-			Value string `json:"value"`
-		}
-
-		var items []Item
-		err = json.Unmarshal(data, &items)
-		if err != nil {
-			fmt.Println(err)
-		}
-		
-		for _, item := range items {
-			cookies[item.Name] = item.Value
-		}
-	} else {
-		for _, cookie := range strings.Split(cookiesStr, ";") {
-			parts := strings.Split(cookie, "=")
-			if len(parts) == 2 {
-			cookies[parts[0]] = parts[1]
-			}
+	for _, cookie := range strings.Split(cookiesStr, ";") {
+		parts := strings.Split(cookie, "=")
+		if len(parts) == 2 {
+		cookies[parts[0]] = parts[1]
 		}
 	}
-
 	
 	return cookies
 }
