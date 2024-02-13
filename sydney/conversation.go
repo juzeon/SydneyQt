0942a9938f6d4a7240cb50322b7131370d0917b0
juzeon <skyjuzheng@gmail.com>
2024-02-13 17:41:53 +0800

    better err for creating conversation

--- sydney/conversation.go
+++ sydney/conversation.go
@@ -6,41 +6,45 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 	"sydneyqt/util"
 	"time"
 )
 
 func (o *Sydney) createConversation() (CreateConversationResponse, error) {
 	client, err := util.MakeHTTPClient(o.proxy, 10*time.Second)
+	emptyResponse := CreateConversationResponse{}
 	if err != nil {
-		return CreateConversationResponse{}, err
+		return emptyResponse, err
 	}
 	req, err := http.NewRequest("GET", o.createConversationURL, nil)
 	if err != nil {
-		return CreateConversationResponse{}, err
+		return emptyResponse, err
 	}
 	for k, v := range o.headersCreateConversation() {
 		req.Header.Set(k, v)
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return CreateConversationResponse{}, err
+		return emptyResponse, err
 	}
 	defer resp.Body.Close()
 	bodyV, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return CreateConversationResponse{}, err
+		return emptyResponse, err
 	}
 	if resp.StatusCode != 200 {
-		return CreateConversationResponse{}, errors.New("Authentication failed: " + string(bodyV))
+		slog.Error("Failed body", "v", string(bodyV))
+		return emptyResponse, errors.New("failed to create the conversation, code: " +
+			strconv.Itoa(resp.StatusCode) + "; please check your proxy settings and your account")
 	}
 	var response CreateConversationResponse
 	err = json.Unmarshal(bodyV, &response)
 	if err != nil {
-		return CreateConversationResponse{}, err
+		return emptyResponse, err
 	}
 	if response.Result.Value == "UnauthorizedRequest" {
-		return CreateConversationResponse{}, errors.New(response.Result.Message)
+		return emptyResponse, errors.New(response.Result.Message)
 	}
 	if value := resp.Header.Get("X-Sydney-Encryptedconversationsignature"); value != "" {
 		response.SecAccessToken = value
