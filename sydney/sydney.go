3bbc5197d61bd117d147fa8e11b3f0ed1fc48673
juzeon <skyjuzheng@gmail.com>
2024-02-16 22:04:54 +0800

    remove unused code

--- sydney/sydney.go
+++ sydney/sydney.go
@@ -19,14 +19,12 @@ type Sydney struct {
 	createConversationURL string
 	bypassServer          string
 
-	optionsSet                []string
-	sliceIDs                  []string
-	locationHints             map[string][]LocationHint
-	allowedMessageTypes       []string
-	headers                   func() map[string]string
-	headersCreateConversation func() map[string]string
-	headersCreateImage        func() map[string]string
-	cookies                   map[string]string
+	optionsSet          []string
+	sliceIDs            []string
+	locationHints       map[string][]LocationHint
+	allowedMessageTypes []string
+	headers             func() map[string]string
+	cookies             map[string]string
 }
 
 func NewSydney(options Options) *Sydney {
@@ -163,42 +161,6 @@ func NewSydney(options Options) *Sydney {
 				"Referrer-Policy":             "origin-when-cross-origin",
 				"x-forwarded-for":             forwardedIP,
 				"Cookie":                      util.FormatCookieString(cookies),
-			}
-		},
-		headersCreateConversation: func() map[string]string {
-			return map[string]string{
-				"authority":                   "www.bing.com",
-				"accept":                      "application/json",
-				"accept-language":             "en-US,en;q=0.9",
-				"cache-control":               "max-age=0",
-				"sec-ch-ua":                   `"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"`,
-				"sec-ch-ua-arch":              `"x86"`,
-				"sec-ch-ua-bitness":           `"64"`,
-				"sec-ch-ua-full-version":      `"110.0.1587.69"`,
-				"sec-ch-ua-full-version-list": `"Chromium";v="110.0.5481.192", "Not A(Brand";v="24.0.0.0", "Microsoft Edge";v="110.0.1587.69"`,
-				"sec-ch-ua-mobile":            `"?0"`,
-				"sec-ch-ua-model":             `""`,
-				"sec-ch-ua-platform":          `"Windows"`,
-				"sec-ch-ua-platform-version":  `"15.0.0"`,
-				"upgrade-insecure-requests":   "1",
-				"user-agent":                  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46",
-				"x-edge-shopping-flag":        "1",
-				"x-forwarded-for":             forwardedIP,
-				"Cookie":                      util.FormatCookieString(cookies),
-			}
-		},
-		headersCreateImage: func() map[string]string {
-			return map[string]string{
-				"authority":                 "www.bing.com",
-				"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-				"accept-language":           "en-US,en;q=0.9",
-				"cache-control":             "no-cache",
-				"referer":                   "https://www.bing.com/search?q=Bing+AI&showconv=1",
-				"upgrade-insecure-requests": "1",
-				"user-agent":                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46",
-				"x-forwarded-for":           forwardedIP,
-				"Sec-Fetch-Dest":            "iframe",
-				"Cookie":                    util.FormatCookieString(cookies),
 			}
 		},
 		cookies: cookies,
